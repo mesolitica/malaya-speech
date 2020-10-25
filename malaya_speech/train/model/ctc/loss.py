881749cad43ce6e6c0e69abc3351c5be3d38294e
husein zolkepli <husein.zol05@gmail.com>
2020-10-25 17:18:25 +0800

    improve ctc loss

--- malaya_speech/train/model/ctc/loss.py
+++ malaya_speech/train/model/ctc/loss.py
@@ -1,6 +1,13 @@
 import tensorflow as tf
 
 
+def dense_to_sparse(dense_tensor, sequence_length):
+    indices = tf.where(tf.sequence_mask(sequence_length))
+    values = tf.gather_nd(dense_tensor, indices)
+    shape = tf.shape(dense_tensor, out_type = tf.int64)
+    return tf.SparseTensor(indices, values, shape)
+
+
 def to_float(x):
     return tf.cast(x, tf.float32)
 
@@ -14,11 +21,13 @@ def ctc_loss(logits, targets, input_lengths, weights_fn = weights_nonzero):
         targets_mask = 1 - tf.to_int32(tf.equal(targets, 0))
         targets_lengths = tf.reduce_sum(targets_mask, axis = 1)
 
-        sparse_targets = tf.keras.backend.ctc_label_dense_to_sparse(
-            targets, targets_lengths
-        )
+        logits = tf.transpose(logits, [1, 0, 2])
+
         xent = tf.nn.ctc_loss(
-            sparse_targets, logits, input_lengths, time_major = False
+            labels = dense_to_sparse(targets, targets_lengths),
+            inputs = logits,
+            sequence_length = input_lengths,
+            ignore_longer_outputs_than_inputs = True,
         )
         weights = weights_fn(targets)
         return tf.reduce_mean(xent), tf.reduce_sum(xent), tf.reduce_sum(weights)
