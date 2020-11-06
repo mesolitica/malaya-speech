a948f6107ad198bb195472ebc12bda59af052ed7
husein zolkepli <husein.zol05@gmail.com>
2020-11-06 10:52:28 +0800

    bump pypi version to 0.0.1.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 setuptools.setup(
     name = __packagename__,
     packages = setuptools.find_packages(),
-    version = '0.0.1',
+    version = '0.0.1.1',
     python_requires = '>=3.6.*',
     description = 'Speech-Toolkit for bahasa Malaysia, powered by Deep Learning Tensorflow.',
     long_description = readme(),
