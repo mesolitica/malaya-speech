5242f0bfaf291ef03333faa2f85c16da8650602f
husein zolkepli <husein.zol05@gmail.com>
2020-12-19 00:37:40 +0800

    fix optimizer

--- malaya_speech/train/optimizer/__init__.py
+++ malaya_speech/train/optimizer/__init__.py
@@ -17,6 +17,9 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import collections
+import six
+
 from tensorflow.python.framework import ops
 from tensorflow.python.ops import array_ops
 from tensorflow.python.ops import control_flow_ops
