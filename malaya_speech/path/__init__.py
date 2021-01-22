95d4ebab2fa45490160436adf874f650e7362078
husein zolkepli <husein.zol05@gmail.com>
2021-01-23 00:36:56 +0800

    fix path

--- malaya_speech/path/__init__.py
+++ malaya_speech/path/__init__.py
@@ -190,7 +190,7 @@ S3_PATH_SPEAKER_VECTOR = {
     },
     'vggvox-v2': {
         'model': 'v1/speaker-vector/pretrained-vggvox-v2.pb',
-        'model': 'v1/speaker-vector/pretrained-vggvox-v2.pb.quantized',
+        'quantized': 'v1/speaker-vector/pretrained-vggvox-v2.pb.quantized',
     },
     'inception-v4': {
         'model': 'v1/speaker-vector/pretrained-inception-v4.pb',
