cb8750b01d8e5b7812c390d8c08d6fcde2f8dbab
husein zolkepli <husein.zol05@gmail.com>
2021-11-10 20:38:07 +0800

    fix load confomer error

--- malaya_speech/supervised/stt.py
+++ malaya_speech/supervised/stt.py
@@ -26,7 +26,9 @@ dummy_sentences = ['tangan aku disentuh lembut', 'sebut perkataan angka']
 
 def transducer_load(model, module, languages, quantized=False, **kwargs):
     splitted = model.split('-')
-    stack = 'stack' == splitted[-2]
+    stack = False
+    if len(splitted) == 3:
+        stack = 'stack' == splitted[-2]
     if stack:
         keys = {'model': 'model.pb'}
         for no, language in enumerate(languages):
