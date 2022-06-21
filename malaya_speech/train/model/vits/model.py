fix masking vits

malaya_speech/train/model/vits/model.py
@@ -539,6 +539,7 @@ class Model(tf.keras.Model):
         self.flow = ResidualCouplingBlock(inter_channels, hidden_channels, 5, 1, 4, gin_channels=gin_channels)
 
         if use_sdp:
+            print('using StochasticDurationPredictor')
             self.dp = StochasticDurationPredictor(hidden_channels, 192, 3, 0.3, 4, gin_channels=gin_channels)
         else:
             self.dp = DurationPredictor(256, 3, 0.1, gin_channels=gin_channels)
@@ -603,8 +604,8 @@ class Model(tf.keras.Model):
 
         if self.use_sdp:
             logw = self.dp(x, x_mask, g=g, reverse=True, noise_scale=noise_scale_w, training=training)
-            w = tf.math.exp(logw) * x_mask * length_scale
-            w_ceil = tf.math.ceil(w)[:, :, 0]
+            w = tf.math.exp(logw) * x_mask
+            w_ceil = tf.math.ceil(w * length_scale)[:, :, 0]
         else:
             logw = self.dp(x, x_mask, g=g, training=training)
             w = tf.nn.relu(tf.math.exp(logw) - 1.0) * x_mask

malaya_speech/train/model/vits/transforms.py
@@ -99,8 +99,10 @@ def unconstrained_rational_quadratic_spline_tf(inputs,
     unnormalized_derivatives = unnormalized_derivatives[tf.tile(tf.expand_dims(inside_interval_mask, -1), [1, 1, 1, d])]
     unnormalized_derivatives = tf.reshape(unnormalized_derivatives, [-1, d])
 
+    inputs_ = inputs[inside_interval_mask]
+
     outputs_, logabsdet_ = rational_quadratic_spline_tf(
-        inputs=inputs[inside_interval_mask],
+        inputs=inputs_,
         unnormalized_widths=unnormalized_widths,
         unnormalized_heights=unnormalized_heights,
         unnormalized_derivatives=unnormalized_derivatives,
@@ -110,12 +112,13 @@ def unconstrained_rational_quadratic_spline_tf(inputs,
         min_bin_height=min_bin_height,
         min_derivative=min_derivative
     )
-    print(outputs_.shape, logabsdet_.shape)
-    outputs_ = tf.reshape(outputs_, tf.shape(inside_interval_mask))
-    logabsdet_ = tf.reshape(logabsdet_, tf.shape(inside_interval_mask))
-
-    outputs = tf.where(inside_interval_mask, outputs_, outputs)
-    logabsdet = tf.where(inside_interval_mask, logabsdet_, logabsdet)
+    indices = tf.where(inside_interval_mask)
+    outputs = tf.tensor_scatter_nd_update(
+        outputs, indices, outputs_, name=None
+    )
+    logabsdet = tf.tensor_scatter_nd_update(
+        logabsdet, indices, logabsdet_, name=None
+    )
 
     return outputs, logabsdet
 