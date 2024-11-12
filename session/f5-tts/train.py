378cc6c4250a3e800439b27db31ed4463d8023f2
huseinzol05 <husein.zol05@gmail.com>
2024-11-12 17:56:00 +0800

    added initial t5-tts

---
+++ session/f5-tts/train.py
@@ -0,0 +1,103 @@
+# training script.
+
+from importlib.resources import files
+
+from f5_tts.model import CFM, DiT, Trainer, UNetT
+from f5_tts.model.dataset import load_dataset
+from f5_tts.model.utils import get_tokenizer
+
+# -------------------------- Dataset Settings --------------------------- #
+
+target_sample_rate = 24000
+n_mel_channels = 100
+hop_length = 256
+win_length = 1024
+n_fft = 1024
+mel_spec_type = "vocos"  # 'vocos' or 'bigvgan'
+
+tokenizer = "pinyin"  # 'pinyin', 'char', or 'custom'
+tokenizer_path = None  # if tokenizer = 'custom', define the path to the tokenizer you want to use (should be vocab.txt)
+dataset_name = "Emilia_Malaysian"
+
+# -------------------------- Training Settings -------------------------- #
+
+exp_name = "F5TTS_Base"  # F5TTS_Base | E2TTS_Base
+
+learning_rate = 2.5e-5
+
+batch_size_per_gpu = 10400  # 8 GPUs, 8 * 38400 = 307200
+batch_size_type = "frame"  # "frame" or "sample"
+max_samples = 64  # max sequences per batch if use frame-wise batch_size. we set 32 for small models, 64 for base models
+grad_accumulation_steps = 1  # note: updates = steps / grad_accumulation_steps
+max_grad_norm = 1.0
+
+epochs = 100  # use linear decay, thus epochs control the slope
+num_warmup_updates = 2000  # warmup steps
+save_per_updates = 50000  # save checkpoint per steps
+last_per_steps = 500  # save last checkpoint per steps
+
+# model params
+if exp_name == "F5TTS_Base":
+    wandb_resume_id = None
+    model_cls = DiT
+    model_cfg = dict(dim=1024, depth=22, heads=16, ff_mult=2, text_dim=512, conv_layers=4)
+elif exp_name == "E2TTS_Base":
+    wandb_resume_id = None
+    model_cls = UNetT
+    model_cfg = dict(dim=1024, depth=24, heads=16, ff_mult=4)
+
+
+# ----------------------------------------------------------------------- #
+
+
+def main():
+    if tokenizer == "custom":
+        tokenizer_path = tokenizer_path
+    else:
+        tokenizer_path = dataset_name
+    vocab_char_map, vocab_size = get_tokenizer(tokenizer_path, tokenizer)
+
+    mel_spec_kwargs = dict(
+        n_fft=n_fft,
+        hop_length=hop_length,
+        win_length=win_length,
+        n_mel_channels=n_mel_channels,
+        target_sample_rate=target_sample_rate,
+        mel_spec_type=mel_spec_type,
+    )
+
+    model = CFM(
+        transformer=model_cls(**model_cfg, text_num_embeds=vocab_size, mel_dim=n_mel_channels),
+        mel_spec_kwargs=mel_spec_kwargs,
+        vocab_char_map=vocab_char_map,
+    )
+
+    trainer = Trainer(
+        model,
+        epochs,
+        learning_rate,
+        num_warmup_updates=num_warmup_updates,
+        save_per_updates=save_per_updates,
+        checkpoint_path=str(files("f5_tts").joinpath(f"../../ckpts/{exp_name}")),
+        batch_size=batch_size_per_gpu,
+        batch_size_type=batch_size_type,
+        max_samples=max_samples,
+        grad_accumulation_steps=grad_accumulation_steps,
+        max_grad_norm=max_grad_norm,
+        wandb_project="CFM-TTS",
+        wandb_run_name=exp_name,
+        wandb_resume_id=wandb_resume_id,
+        last_per_steps=last_per_steps,
+        log_samples=True,
+        mel_spec_type=mel_spec_type,
+    )
+
+    train_dataset = load_dataset(dataset_name, tokenizer, mel_spec_kwargs=mel_spec_kwargs)
+    trainer.train(
+        train_dataset,
+        resumable_with_seed=666,  # seed for shuffling dataset
+    )
+
+
+if __name__ == "__main__":
+    main()
--- session/whisper/run-large-v3-turbo.sh
+++ session/whisper/run-large-v3-turbo.sh
@@ -1,7 +1,7 @@
-WANDB_PROJECT=malaysian-whisper-large-v3 \
+WANDB_PROJECT=malaysian-whisper-large-v3-turbo-v2 \
 torchrun --nproc_per_node 2 \
 -m whisper \
---model_name_or_path "openai/whisper-large-v3" \
+--model_name_or_path "openai/whisper-large-v3-turbo" \
 --train_dataset_name "mosaic-stt" \
 --eval_steps 1000 \
 --save_steps 100 \
@@ -11,12 +11,14 @@ torchrun --nproc_per_node 2 \
 --logging_steps 1 \
 --save_total_limit 3 \
 --num_train_epochs 3 \
---per_device_train_batch_size 16 \
+--per_device_train_batch_size 24 \
 --gradient_accumulation_steps 2 \
 --per_device_eval_batch_size 4 \
---output_dir "./large" \
+--output_dir "malaysian-whisper-large-v3-turbo-v2" \
 --do_train \
 --gradient_checkpointing \
 --predict_with_generate \
 --max_label_length 440 \
---bf16
+--bf16 \
+--dataloader_num_workers 3 \
+--dataloader_prefetch_factor 4
